55975227e1255fa218c5b6f6883f12d264f3cee5
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2018-07-16 22:30:44 +0200

    Bootsrap added

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
