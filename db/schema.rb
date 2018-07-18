9c16c9d9b2267600ec716234693b934b747bb119
Dominik Zduńczyk <dominik.zdunczyk@goodsoft.pl>
2018-07-18 16:59:56 +0200

    Fixed view of posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_142044) do
+ActiveRecord::Schema.define(version: 2018_07_18_145143) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
